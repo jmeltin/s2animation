bae07640aef1d14fc1debd2d2b1f4e646f3c54b5
Jeremy Meltingtallow <jeremy@seven2.com>
2015-06-25 14:41:49 -0700

    init

---
+++ js/s2anim.js
@@ -0,0 +1,58 @@
+function s2anim(id) {
+	var _target = document.getElementById(id);
+
+	if(_target == null)
+		throw "Target ID is invalid";
+	
+	var _targetOrigClassList = _target.className,
+		_commandList = [],
+		_delayList = [],
+		_currentCommand = "",
+		_delay = null;
+
+	_target.addEventListener('webkitTransitionEnd', nextCommand, false);
+
+
+	this.add = function(name, delay)
+	{
+		if(isNaN(delay))
+			throw delay + " is invalid";
+		_commandList.push(name);
+		_delayList.push(delay);
+		return this;
+	}
+
+	this.start = function()
+	{
+		nextCommand();
+	}
+
+	function switchTransition()
+	{
+		var targetClass = _targetOrigClassList + " " + _currentCommand;
+		_target.setAttribute("class", targetClass);
+		clearDelay();
+	}
+
+	function clearDelay()
+	{
+		if(_delay != null) {
+			window.clearTimeout(_delay);
+			_delay = null;
+		}
+	}
+
+	function nextCommand()
+	{
+		if(_commandList.length != 0) {
+			_currentCommand = _commandList.shift();
+			var time = _delayList.shift() * 1000;
+			_delay = setTimeout(switchTransition, time)
+		}
+	}
+}
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,8 @@
+new s2anim("id1")
+	.add("moveRight", 4)
+	.add("moveDown", 2)
+	.start();
+
+new s2anim("circle")
+	.add("moveDown", 10)
+	.start();
