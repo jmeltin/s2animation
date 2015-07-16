ae63c86b0d811cc88f66a45dc1169b5f597588ee
Jeremy Meltingtallow <jeremy@seven2.com>
2015-07-16 15:07:14 -0700

    browser compatibility

--- example/js/s2anim.js
+++ example/js/s2anim.js
@@ -4,20 +4,12 @@ function s2anim(id) {
 	if(_target == null)
 		throw "Target ID is invalid";
 	
-	var _commandList = [],
-		_delayList = [],
-		_currentCommand = "",
-		_delay = null;
+	var _commandList = [];
+	addListener(_target, nextCommand);
 
-	_target.addEventListener('animationend', nextCommand, false);
-
-
-	this.add = function(name, delay)
+	this.add = function(name)
 	{
-		if(isNaN(delay))
-			throw delay + " is invalid";
 		_commandList.push(name);
-		_delayList.push(delay);
 		return this;
 	}
 
@@ -26,22 +18,22 @@ function s2anim(id) {
 		nextCommand();
 	}
 
-	function switchTransition()
-	{
-		_target.setAttribute("class", _target.className += " " + _currentCommand);
-	}
-
 	function nextCommand(event)
 	{
 		if(_commandList.length != 0) {
-			_currentCommand = _commandList.shift();
-			var time = _delayList.shift() * 1000;
-			setTimeout(switchTransition, time)
+			_target.setAttribute("class", _target.className += " " + _commandList.shift())
+		}
+	}
+
+	function addListener(element, func)
+	{
+		var events=['animationend','webkitAnimationEnd','oanimationend','MSAnimationEnd'];
+		for(var i in events) {
+			element.addEventListener(events[i], func, false);
 		}
 	}
 }
 
 
 
-
-
+		
